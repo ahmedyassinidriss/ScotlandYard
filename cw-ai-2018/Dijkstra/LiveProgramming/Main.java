ef3c6727afafd708b266c17e1846c2737aa9396e
Davis Offei <do17096@my.bristol.ac.uk>
2018-03-29 18:42:56 +0100

    comment

---
+++ cw-ai-2018/Dijkstra/LiveProgramming/Main.java
@@ -0,0 +1,36 @@
+// COMS10009 Live Programming Example Code
+// Main: runs Prim's and Dijkstra's algorithms on simple graphs
+public class Main {
+  
+  public static void main(String[] args) {
+
+	//instances for loading and saving simple graph files
+	Reader reader = new Reader();
+	Writer writer = new Writer();
+		
+	try {
+	  //load input graph
+	  reader.read("c:/data/graph.txt"); //TODO: change input path
+	  Graph<Integer,Integer> graph = reader.graph();
+	  System.out.println("Graph read.");
+
+   	  //calculate single-source-all-shortest-paths and output one route
+	  DijkstraCalculator dijk = new DijkstraCalculator(graph);
+	  Graph<Integer,Integer> result = dijk.getResult(3,159); //from node 3 to node 159
+				
+	  //output route as graph, use 'java -jar GraphViewer.jar route.txt' to view
+	  writer.setGraph(result);
+	  writer.write("c:/data/route.txt"); //TODO: change output path
+	  System.out.println("Shortest route graph written.");
+	
+	  //calculate and output minimum spanning tree 
+	  PrimCalculator prim = new PrimCalculator(graph);
+	  result = prim.getResult(1);
+				
+	  //output minimum spanning tree as graph, use 'java -jar GraphViewer.jar mst.txt' to view
+	  writer.setGraph(result);
+	  writer.write("c:/data/mst.txt"); //TODO: change output path
+	  System.out.println("Minimum spanning tree written.");
+	} catch (Exception e) {
+	  //TODO: handle exceptions
+} } }
---
+++ cw-ai-2018/cw-ai/src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/AIdo.java
@@ -0,0 +1,147 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import com.sun.javafx.collections.MapAdapterChange;
+import uk.ac.bris.cs.gamekit.graph.Edge;
+import uk.ac.bris.cs.scotlandyard.ai.ManagedAI;
+import uk.ac.bris.cs.scotlandyard.ai.PlayerFactory;
+import uk.ac.bris.cs.scotlandyard.model.*;
+
+
+//copy from ScotlandYardModel
+
+import java.util.*;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singletonList;
+import static java.util.Collections.unmodifiableCollection;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Collections.unmodifiableSet;
+import static java.util.Objects.requireNonNull;
+import static uk.ac.bris.cs.scotlandyard.model.Colour.BLACK;
+import static uk.ac.bris.cs.scotlandyard.model.Ticket.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+
+
+import uk.ac.bris.cs.gamekit.graph.Edge;
+import uk.ac.bris.cs.gamekit.graph.Graph;
+import uk.ac.bris.cs.gamekit.graph.Node;
+import uk.ac.bris.cs.gamekit.graph.ImmutableGraph;
+
+
+//
+
+
+
+
+
+
+@ManagedAI("AIdo")
+public class AIdo implements PlayerFactory {
+
+	// TODO create a new player here
+	@Override
+	public Player createPlayer(Colour colour) {
+		return new MyPlayer();
+	}
+
+	// TODO A sample player that selects a random move
+	private static class MyPlayer implements Player {
+	    //penalise for certain moves, include DoubleMove
+
+
+
+
+        Double transportScore(ScotlandYardView view, int locationToScore) {
+            // Check for how many Transport options at the location
+            Double numberOfTransport = 0.0;
+
+            boolean hasTaxi = false;
+            boolean hasBus = false;
+            boolean hasUnderground = false;
+            boolean hasFerry = false;
+
+
+            for (Edge<Integer, Transport> edge : view.getGraph().getEdgesFrom(view.getGraph().getNode(locationToScore))) {
+                if (edge.data().equals(Transport.TAXI)) hasTaxi = true;
+                if (edge.data().equals(Transport.BUS)) hasBus = true;
+                if (edge.data().equals(Transport.UNDERGROUND)) hasUnderground = true;
+                if (edge.data().equals(Transport.FERRY)) hasFerry = true;
+            }
+
+            if (hasTaxi) numberOfTransport++;
+            if (hasBus) numberOfTransport++;
+            if (hasUnderground) numberOfTransport++;
+            if (hasFerry) numberOfTransport++;
+
+
+
+            return numberOfTransport;
+        };
+
+
+
+
+
+
+        Double numberOfMovesScore (ScotlandYardView view, int locationToScore) {
+    // Check for how many edges from the location
+            //Double numberOfMoves = (Double) view.getGraph().getEdgesFrom(view.getGraph().getNode(locationToScore)).size(); //ask TA or Forum
+
+            Double numberOfMoves = 0.0;
+        for (Edge<Integer, Transport> edge : view.getGraph().getEdgesFrom(view.getGraph().getNode(locationToScore))) {
+            numberOfMoves++;
+        }
+        return numberOfMoves;
+    };
+
+
+        @Override
+		public void makeMove(ScotlandYardView view, int locationOfMrX, Set<Move> moves,
+				Consumer<Move> callback) {
+
+            Map<Move,Double> ScoresMap = new HashMap<>(); // Map of each move and its (single value) Score
+
+
+            for (Move m : moves) {
+              //  ScoresMap.put(m, ScoresMap.get(m).add(numberOfMovesScore(view, ((TicketMove) m).destination()))); //returns previous Map with List(using move as key) with new score
+                if (m instanceof TicketMove){
+                    ScoresMap.put(m, numberOfMovesScore(view, ((TicketMove) m).destination()));}
+            }
+
+
+            for (Move m : moves) {
+
+                if (m instanceof TicketMove) {
+                    Double newScore = (ScoresMap.get(m) + transportScore(view, ((TicketMove) m).destination()));
+                    ScoresMap.put(m, newScore);
+                }
+            }
+
+
+
+            //select optimalMove
+            Move optimalMove = null;
+            for (Move m2 : moves) {
+                if (ScoresMap.get(m2) == Collections.max(ScoresMap.values())) optimalMove= m2;
+            }
+
+			callback.accept(optimalMove);
+
+		}
+	}
+}
